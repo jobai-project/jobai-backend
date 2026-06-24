be957926442f35178dde5fd3d0346d4713464893
Minjoo Kim <minpretty100@naver.com>
2026-06-24 22:57:31 +0900

    fix: allow bean override in spring context test

--- src/test/java/com/jobai/backend/BackendApplicationTests.java
+++ src/test/java/com/jobai/backend/BackendApplicationTests.java
@@ -1,31 +1,18 @@
 package com.jobai.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = BackendApplicationTests.TestApplication.class)
+@SpringBootTest(
+		classes = BackendApplication.class,
+		properties = "spring.main.allow-bean-definition-overriding=true"
+)
 @ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@SpringBootConfiguration
-	@EnableAutoConfiguration
-	@ComponentScan(
-			basePackages = "com.jobai.backend",
-			excludeFilters = {
-					@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BackendApplication.class),
-					@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.jobai\\.backend\\..*Test.*")
-			}
-	)
-	static class TestApplication {
 	}
 
 }
